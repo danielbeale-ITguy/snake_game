38439810d89b8766f23c67ef0423b747679d7dcf
danielbeale-ITguy <danielbeale719@gmail.com>
2025-10-05 20:41:10 +1100

    moved outside grid for loop to the class, and just trying out how to update the snake position

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 import pygame
 from cloudinit.sources.DataSourceConfigDrive import LABEL_TYPES
+from gi.types import snake_case
 from numpy.array_api import square
+from oauthlib.uri_validate import segment_nz_nc
 from pexpect.screen import screen
-
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-RED = (255,0,0)
-LAVENDAR = (230,230,250)
+from pkg_resources import working_set
 
 
 
 
-pygame.init()
-screen_height = 700
-screen_width = 700
-screen = pygame.display.set_mode((screen_height,screen_width))
 
-square_size = 25
-
-
-
-running = True
 
 
 boarder_data = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
@@ -52,54 +41,91 @@ boarder_data = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
 
-rect_list = []
 
-y = 0
-for row in boarder_data:
-    x = 0
-    for tile in row:
-        if tile == 1:
-            rect_list.append((x,y,square_size,square_size))
-        x += square_size
-    x=0
-    y += square_size
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+LAVENDAR = (230,230,250)
+GREEN = (0,255,0)
 
 
 
+pygame.init()
+screen_height = 700
+screen_width = 700
+screen = pygame.display.set_mode((screen_height,screen_width))
 
-print(rect_list)
-
-
-
-
-
-
-
+square_size = 25
+running = True
 
 
 
+class boarder:
+    def __init__(self,screen,screen_height,square_size,color,world_data):
+        self.screen = screen
+        self.screen_height = screen_height
+        self.square_size = square_size
+        self.color = color
+        self.second_color = WHITE
+        self.line_color = RED
+        self.grid_list = []
+        self.world_data = world_data
+
+#defines the grid out Rects using double for loop
+
+    def grid(self):
+        y_count = 0
+        for row in self.world_data:
+            x_rect = 0
+            for column in row:
+                if column == 1:
+                    self.grid_list.append((x_rect,y_count,self.square_size,self.square_size))
+                x_rect += self.square_size
+            x_rect = 0
+            y_count += self.square_size
+
+    # draws the Rect list to screen
+    def draw_grid(self):
+        for square_to_draw in self.grid_list:
+                pygame.draw.rect(self.screen, self.second_color, (square_to_draw))
+
+# This is just to act a guide, no other funciton
+    def line_guide(self):
+        for line in range(0,self.screen_height,self.square_size):
+            pygame.draw.line(self.screen,self.line_color,(line,0),(line,self.screen_height))
+            pygame.draw.line(self.screen,self.line_color, (0,line),(self.screen_height,line))
+
+game_run = boarder(screen,screen_height,square_size,WHITE,boarder_data)
 
 
+x = 50
+snake_y = 50
 
 
 
 while running:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-
-
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    x += 50
 
 
         screen.fill(LAVENDAR)
-        for i in rect_list:
-            pygame.draw.rect(screen,RED,(i))
+
+
+# Testing out how to draw and update snake, can use below, or you can define the rect outside of the loop and use the snake_rect.x += 5
+        snake_rect = pygame.Rect(x, snake_y, square_size, square_size)
+        pygame.draw.rect(screen,GREEN,(snake_rect))
+
 
         pygame.display.update()
-    #QUITS the game
+#QUITS the game
 pygame.quit()
-
 
 
 
